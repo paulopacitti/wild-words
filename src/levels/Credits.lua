5f5bab16d6cf17258254addbe49ee53c9a962830
paulopacitti <ppacitti@outlook.com>
2023-04-14 20:14:40 -0300

    Refactors Credits level with better Lua code (man, this repo is a mess, fixing it writing better Lua code)

--- src/levels/Credits.lua
+++ src/levels/Credits.lua
@@ -1,36 +1,34 @@
-local Credits = {}
 local Gamestate = require("libs.gamestate")
-
 local Text = require("src.objects.Text")
-local SoundSystem = require("src.systems.SoundSystem")
+local Credits = {}
 
 function Credits:init()
-  slides = {}
-  slide_index = 1
-  
-  createdBy = {}
-  createdBy[1] = Text:new({x = 0, y = 230 }, "credits_title", "center", "created by")
-  createdBy[2] = Text:new({x = 0, y = 280 }, "credits_label", "center", "paulo pacitti")
-  
-  musicBy = {}
-  musicBy[1] = Text:new({x = 0, y = 230 }, "credits_title", "center", "music by")
-  musicBy[2] = Text:new({x = 0, y = 280 }, "credits_label", "center", "paulo pacitti")
-  
-  artBy = {}
-  artBy[1] = Text:new({x = 0, y = 230 }, "credits_title", "center", "art by")
-  artBy[2] = Text:new({x = 0, y = 280 }, "credits_label", "center", "gabriel teston")
+  self.slides = {}
+  self.slide_index = 1
 
-  madeWith = {}
-  madeWith[1] = Text:new({x = 0, y = 230 }, "credits_title", "center", "made with")
-  madeWith[2] = Text:new({x = 0, y = 280 }, "credits_label", "center", "Love2D")
-  madeWith[3] = Text:new({x = 0, y = 305 }, "credits_label", "center", "middleclass")
-  madeWith[4] = Text:new({x = 0, y = 325 }, "credits_label", "center", "hump.gamestate")
-  madeWith[5] = Text:new({x = 0, y = 345 }, "credits_label", "center", "wave")
-  
-  slides[1] = createdBy
-  slides[2] = musicBy
-  slides[3] = artBy
-  slides[4] = madeWith
+  local createdBy = {}
+  createdBy[1] = Text:new({ x = 0, y = 230 }, "credits_title", "center", "created by")
+  createdBy[2] = Text:new({ x = 0, y = 280 }, "credits_label", "center", "paulo pacitti")
+
+  local musicBy = {}
+  musicBy[1] = Text:new({ x = 0, y = 230 }, "credits_title", "center", "music by")
+  musicBy[2] = Text:new({ x = 0, y = 280 }, "credits_label", "center", "paulo pacitti")
+
+  local artBy = {}
+  artBy[1] = Text:new({ x = 0, y = 230 }, "credits_title", "center", "art by")
+  artBy[2] = Text:new({ x = 0, y = 280 }, "credits_label", "center", "gabriel teston")
+
+  local madeWith = {}
+  madeWith[1] = Text:new({ x = 0, y = 230 }, "credits_title", "center", "made with")
+  madeWith[2] = Text:new({ x = 0, y = 280 }, "credits_label", "center", "Love2D")
+  madeWith[3] = Text:new({ x = 0, y = 305 }, "credits_label", "center", "middleclass")
+  madeWith[4] = Text:new({ x = 0, y = 325 }, "credits_label", "center", "hump.gamestate")
+  madeWith[5] = Text:new({ x = 0, y = 345 }, "credits_label", "center", "wave")
+
+  self.slides[1] = createdBy
+  self.slides[2] = musicBy
+  self.slides[3] = artBy
+  self.slides[4] = madeWith
 end
 
 -- when re-entering the level, start from scratch
@@ -39,8 +37,8 @@ function Credits:enter()
 end
 
 function Credits:draw()
-  for key, text in ipairs(slides[slide_index]) do
-    text:draw()
+  for key, slide in ipairs(self.slides[self.slide_index]) do
+    slide:draw()
   end
 end
 
@@ -49,13 +47,13 @@ function Credits:update(dt)
 end
 
 function Credits:keypressed(key)
- if key == "escape" then
+  if key == "escape" then
     Gamestate.pop()
   elseif key == "return" then
-    if slide_index >= 4 then
+    if self.slide_index >= 4 then
       Gamestate.pop()
     else
-      slide_index = slide_index + 1
+      self.slide_index = self.slide_index + 1
     end
   end
 end
